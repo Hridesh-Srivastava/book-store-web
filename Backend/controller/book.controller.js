a4a7123fa87327d5afb4ac77592415d5205c56a4
ShivanshKothari <shivanshkothari2004@gmail.com>
2024-09-27 10:24:57 +0530

    Added book post controller

--- Backend/controller/book.controller.js
+++ Backend/controller/book.controller.js
@@ -9,3 +9,14 @@ export const getBook = async(req, res) => {
         res.status(500).json(error);
     }
 };
+
+export const addBook = async(req,res) => {
+    try {
+        const bookData = await Book.create(req.body);
+        res.status(200).json(bookData);
+
+    } catch (error) {
+        console.log("Error while adding book", error);
+        res.status(500).json(error);
+    }
+}
--- Backend/route/book.route.js
+++ Backend/route/book.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getBook } from "../controller/book.controller.js";
+import { addBook, getBook } from "../controller/book.controller.js";
 
 const router = express.Router();
 
 router.get("/", getBook);
+
+router.post("/", addBook)
 
 export default router;
